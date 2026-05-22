ac2f778b75280b967de49c26b4a4e35cca9acfb5
lytic11 <lytic11@gmail.com>
2026-05-22 20:09:25 +0300

    TemplateView

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
+from hexlet_django_blog.article.views import IndexView
 
-from hexlet_django_blog.article import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path('', IndexView.as_view(), name='index'),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -3,13 +3,16 @@ from django.shortcuts import render
 # Create your views here.
 # hexlet_django_blog/article/views.py
 from django.http import HttpResponse
+from django.views import View
+from django.shortcuts import render
 
 
-def index(request):
-    return render(
-        request,
-        "articles/index.html",
-        context={
-            "app_name": "hexlet_django_blog.article",
-        },
-    )
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(
+            request,
+            "articles/index.html",
+            context={
+                "app_name": "hexlet_django_blog.article",
+            },
+        )
