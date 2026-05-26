a0fab731cd4366e613e3cd1586273a2c345ef187
lytic11 <lytic11@gmail.com>
2026-05-26 18:59:36 +0300

    Configure Django Admin for Article model

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -7,3 +7,8 @@ class Article(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+
+
+    def __str__(self):
+        return self.name
