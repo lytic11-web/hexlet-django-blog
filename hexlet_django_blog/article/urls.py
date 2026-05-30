6f1a46767da19a4037453e37643cc19b85b4d55b
lytic11 <lytic11@gmail.com>
2026-05-30 11:36:37 +0300

    Add article update (edit) view

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
-from hexlet_django_blog.article.views import IndexView, ArticleView, ArticleFormCreateView
+from hexlet_django_blog.article.views import (
+    IndexView,
+    ArticleView,
+    ArticleFormCreateView,
+    ArticleFormEditView,
+)
+
 
 urlpatterns = [
     path("", IndexView.as_view(), name="articles"),
-    path("<int:id>/", ArticleView.as_view(), name="article"),
     path("create/", ArticleFormCreateView.as_view(), name="articles_create"),
+    path("<int:id>/edit/", ArticleFormEditView.as_view(), name="articles_update"),  # ← ДО <int:id>
+    path("<int:id>/", ArticleView.as_view(), name="article"),
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -42,3 +42,29 @@ class ArticleFormCreateView(View):
             messages.success(request, "Статья успешно создана!")
             return redirect(reverse("articles"))
         return render(request, "articles/create.html", {"form": form})
+
+
+class ArticleFormEditView(View):
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = get_object_or_404(Article, id=article_id)
+        form = ArticleForm(instance=article)
+        return render(
+            request,
+            "articles/update.html",
+            {"form": form, "article_id": article_id},
+        )
+
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get("id")
+        article = get_object_or_404(Article, id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Статья успешно обновлена!")
+            return redirect("articles")
+        return render(
+            request,
+            "articles/update.html",
+            {"form": form, "article_id": article_id},
+        )
